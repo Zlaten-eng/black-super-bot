cf631407f075350d27ac7d277d6e127266e2dc17
Zlaten-eng <ronojepsat70@gmail.com>
2025-05-04 12:23:52 +0300

    Update set.js

--- set.js
+++ set.js
@@ -4,15 +4,15 @@ There's no need to configure this if you're deploying via Heroku — just set th
 
 const sessionName = 'session';
 const session = process.env.SESSION || '';
-const autobio = process.env.AUTOBIO || 'FALSE';
+const autobio = process.env.AUTOBIO || 'TRUE';
 const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
 const autoviewstatus = process.env.AUTOVIEW_STATUS || 'TRUE';
 const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE';
 const prefix = process.env.PREFIX || '';
 const appname = process.env.APP_NAME || '';
 const herokuapi = process.env.HEROKU_API;
-const gptdm = process.env.GPT_INBOX || 'FALSE';
-const mode = process.env.MODE || 'PUBLIC';
+const gptdm = process.env.GPT_INBOX || 'TRUE';
+const mode = process.env.MODE || 'PRIVATE';
 const anticall = process.env.AUTOREJECT_CALL || 'TRUE';
 const botname = process.env.BOTNAME || '𝐁𝐋𝐀𝐂𝐊𝐌𝐀𝐂𝐇𝐀𝐍𝐓 𝐁𝐎𝐓';
 const antibot = process.env.ANTIBOT || 'FALSE';
@@ -24,7 +24,7 @@ const menu = process.env.MENU_TYPE || 'VIDEO';
 const DevRaven = dev.split(",");
 const badwordkick = process.env.BAD_WORD_KICK || 'FALSE';
 const bad = process.env.BAD_WORD || 'fuck';
-const autoread = process.env.AUTOREAD || 'FALSE';
+const autoread = process.env.AUTOREAD || 'TRUE';
 const antidel = process.env.ANTIDELETE || 'TRUE';
 const admin = process.env.ADMIN_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗿𝗲𝘀𝗲𝗿𝘃𝗲𝗱 𝗳𝗼𝗿 𝗔𝗱𝗺𝗶𝗻𝘀!';
 const group = process.env.GROUP_ONLY_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝗚𝗿𝗼𝘂𝗽𝘀!';
